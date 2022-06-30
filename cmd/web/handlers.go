b954f31470bb5c886873642aed7b5fa2f1d1e367
SmoothWay <erakarbay@gmail.com>
2022-07-01 00:17:58 +0600

    added page with form to send post request and create snippet, data validation for form values

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/SmoothWay/snippetBox/pkg/models"
 )
@@ -21,11 +23,15 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
+		app.infoLog.Println("err & id")
+		app.infoLog.Println(err)
 		app.notFound(w)
 		return
 	}
 	s, err := app.snippets.Get(id)
 	if err == models.ErrNoRecord {
+		app.infoLog.Println("errnorecord")
+		app.infoLog.Println(err)
 		app.notFound(w)
 		return
 	} else if err != nil {
@@ -36,13 +42,42 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	app.render(w, r, "create-page.html", nil)
 }
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
-	title := "An silent pond"
-	content := "An silent pond...\nA frog jumps into the pond,\nsplash! Silence again."
-	expires := "7"
+	err := r.ParseForm()
+
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
+
+	errors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be blank"
+	}
+
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "Thist field cannot be blank"
+	} else if expires != "365" && expires != "7" &&
+		expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+
+	if len(errors) > 0 {
+		fmt.Fprint(w, errors)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 
@@ -50,6 +85,6 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 
 }
